ba6006aceb9706ed40a155da0c2c0a1715edf7d1
Vitalii Stankov <36083562+vitaliistankov@users.noreply.github.com>
2021-04-09 20:05:18 +0300

    Left sidebar reusable component

--- backend/user-update.php
+++ backend/user-update.php
@@ -83,54 +83,10 @@
         <!--Side Nav-->
         <div id="layoutSidenav">
             <div id="layoutSidenav_nav">
-                <nav class="sidenav shadow-right sidenav-light">
-                    <div class="sidenav-menu">
-                        <div class="nav accordion" id="accordionSidenav">
-                            <a class="nav-link collapsed pt-4" href="index.html">
-                                <div class="nav-link-icon"><i data-feather="activity"></i></div>
-                                Dashboard
-                            </a>
-                            <a class="nav-link collapsed" href="javascript:void(0);" data-toggle="collapse" data-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts"><div class="nav-link-icon"><i data-feather="layout"></i></div>
-                                Posts
-                                <div class="sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                            </a>
-                            <div class="collapse" id="collapseLayouts" data-parent="#accordionSidenav">
-                                <nav class="sidenav-menu-nested nav accordion" id="accordionSidenavLayout">
-                                    <a class="nav-link" href="all-post.html">All Posts</a>
-                                    <a class="nav-link" href="add-new.html">Add New Post</a>
-                                </nav>
-                            </div>
-
-                            <a class="nav-link" href="categories.html" ><div class="nav-link-icon"><i data-feather="chevrons-up"></i></div>
-                                Categories
-                            </a>
-
-                            <a class="nav-link" href="pages.html" ><div class="nav-link-icon"><i data-feather="book-open"></i></div>
-                                Pages
-                            </a>
-
-                            <a class="nav-link" href="comments.html" ><div class="nav-link-icon"><i data-feather="package"></i></div>
-                                Comments
-                            </a>
-
-                            <a class="nav-link" href="messages.html" ><div class="nav-link-icon"><i data-feather="mail"></i></div>
-                                Messages
-                            </a>
-
-                            <a class="nav-link" href="profile.html" ><div class="nav-link-icon"><i data-feather="user"></i></div>
-                                Profile
-                            </a>
-                        </div>
-                    </div>
-
-                    <div class="sidenav-footer">
-                        <div class="sidenav-footer-content">
-                            <div class="sidenav-footer-subtitle">Logged in as:</div>
-                            <div class="sidenav-footer-title">Md. A. Barik</div>
-                        </div>
-                    </div>
-
-                </nav>
+                <?php 
+                    $curr_page = basename(__FILE__);
+                    require_once("./includes/left-sidebar.php");
+                ?>
             </div>
 
 
